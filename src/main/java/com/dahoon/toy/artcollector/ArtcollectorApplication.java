41a79d1a1e862177587227d0e21a6076eff075b1
dahoon kang <dahoon7151@gmail.com>
2024-05-19 18:11:00 +0900

    초기 설정

---
+++ src/main/java/com/dahoon/toy/artcollector/ArtcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.dahoon.toy.artcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtcollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtcollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dahoon/toy/artcollector/ArtcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahoon.toy.artcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtcollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
