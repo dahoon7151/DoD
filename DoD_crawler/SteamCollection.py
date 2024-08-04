11276010a9f295eee4fbff441e6d05dc17e7a264
dahoon kang <dahoon7151@gmail.com>
2024-08-04 20:07:34 +0900

    스팀 크롤러 구현 완료

--- DoD_crawler/SteamCollection.py
+++ DoD_crawler/SteamCollection.py
@@ -1,21 +1,31 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 class Steam:
     def __init__(self):
         self.client = MongoClient('mongodb://localhost:27017/')
         self.db = self.client['DoD_Game']
         self.collection = self.db['Crawller_Steam']
-        self.collection.create_index([('id', 1)], unique=True)  # 인덱싱 수정 필요
+        self.collection.create_index([('id', 1)], unique=True)
+        self.existing_ids = self._get_existing_ids()
+    
+    def _get_existing_ids(self):
+        return set(doc['id'] for doc in self.collection.find({}, {'id': 1}))
 
     def insertMany(self, documents):
-        print(documents)
-        result = self.collection.insert_many(documents)
+        new_documents = [doc for doc in documents if doc['id'] not in self.existing_ids]
 
-        if result.acknowledged:
-            print("크롤링 데이터 DB 저장 성공")
-            print("데이터 ID:", result.inserted_ids)
+        if new_documents:
+            try:
+                result = self.collection.insert_many(new_documents, ordered=False)
+                print("크롤링 데이터 DB 저장 성공")
+                print("데이터 ID:", result.inserted_ids)
+
+                # 새로 삽입된 문서의 id를 existing_ids에 추가
+                self.existing_ids.update(doc['id'] for doc in new_documents)
+            except Exception as e:
+                print("크롤링 데이터 DB 저장 중 알 수 없는 오류 발생:", e)
         else:
-            print("크롤링 데이터 DB 저장 실패")
+            print("삽입할 새로운 데이터가 없습니다.")
 
     def insertOne(self, item):
         self.collection.insert_one(item)
--- DoD_crawler/SteamCrawller.py
+++ DoD_crawler/SteamCrawller.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 # from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -29,48 +29,53 @@ wait = WebDriverWait(driver, 30)
 img_selector = 'div.col.search_capsule > img'
 rate_selector = 'div.responsive_search_name_combined > div.col.search_reviewscore.responsive_secondrow > span'
 
-# 스크롤 (일단 10회)
+# 스크롤
 scroll = driver.find_element(By.CSS_SELECTOR, 'body')
-for i in range(15):
-    scroll.send_keys(Keys.END)
-    time.sleep(2)
+for i in range(20):
+    wrap = wait.until(EC.presence_of_element_located((By.ID, "search_resultsRows")))
+    game_info = wrap.find_elements(By.TAG_NAME,'a')
+    log.info('a 태그 탐색 완료')
 
-wrap = wait.until(EC.presence_of_element_located((By.ID, "search_resultsRows")))
-game_info = wrap.find_elements(By.TAG_NAME,'a')
-log.info('a 태그 탐색 완료')
-
-element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#search_resultsRows > a:nth-child(1) > div.col.search_capsule > img')))
-ele_list = []
-if element:
-    for game in game_info:
-        ele = {}
-        ele['id'] = game.get_attribute('data-ds-appid')
-        ele['title'] = game.find_element(By.CLASS_NAME, 'title').text
-        ele['url'] = game.get_attribute('href')
-        try:
-            ele['img'] = game.find_element(By.CSS_SELECTOR, img_selector).get_attribute('src')
-        except:
-            ele['img'] = "사진 없음"
-        try:
-            ele['release'] = game.find_element(By.CLASS_NAME, 'col.search_released.responsive_secondrow').text
-        except:
-            ele['release'] = "출시일 미정"
-        try:
-            ele['rate'] = game.find_element(By.CSS_SELECTOR, rate_selector).get_attribute('data-tooltip-html')
-        except:
-            ele['rate'] = "평가 없음"
-        try:
-            ele['price'] = game.find_element(By.CLASS_NAME, 'discount_final_price').text
-        except:
-            ele['price'] = "가격 미측정"
-        try:
-            ele['discount'] = game.find_element(By.CLASS_NAME, 'discount_pct').text
-        except:
-            ele['discount'] = "정가"
-
-        ele_list.append(ele)
-
-    db.insertMany(ele_list)
+    element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#search_resultsRows > a:nth-child(1) > div.col.search_capsule > img')))
     ele_list = []
+    if element:
+        for game in game_info:
+            ele = {}
+            ele['id'] = game.get_attribute('data-ds-appid')
+            if ele['id'] in db.existing_ids:
+                continue  # 중복 체크
+            ele['title'] = game.find_element(By.CLASS_NAME, 'title').text
+            ele['url'] = game.get_attribute('href')
+            try:
+                ele['img'] = game.find_element(By.CSS_SELECTOR, img_selector).get_attribute('src')
+            except:
+                ele['img'] = "사진 없음"
+            try:
+                ele['release'] = game.find_element(By.CLASS_NAME, 'col.search_released.responsive_secondrow').text
+            except:
+                ele['release'] = "출시일 미정"
+            try:
+                ele['rate'] = game.find_element(By.CSS_SELECTOR, rate_selector).get_attribute('data-tooltip-html')
+            except:
+                ele['rate'] = "평가 없음"
+            try:
+                ele['price'] = game.find_element(By.CLASS_NAME, 'discount_final_price').text
+            except:
+                ele['price'] = "가격 미측정"
+            try:
+                ele['discount'] = game.find_element(By.CLASS_NAME, 'discount_pct').text
+            except:
+                ele['discount'] = "정가"
+
+            ele_list.append(ele)
+
+        if ele_list:
+            db.insertMany(ele_list)
+            db.existing_ids.update(ele['id'] for ele in ele_list) # 중복id 리스트 업데이트
+        ele_list = []
+        
+    scroll.send_keys(Keys.END)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[data-ds-appid]'))) # time.sleep 도 고려
+    
 
 driver.quit()
