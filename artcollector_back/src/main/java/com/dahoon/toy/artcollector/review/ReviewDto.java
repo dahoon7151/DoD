8f49c8c1062ac01997f3353562bc65d9c4306b1b
dahoon kang <dahoon7151@gmail.com>
2025-05-11 20:01:21 +0900

    feat: ReviewDto writerName, lastModifiedTime 추가
    - toDto 내부 로직 추가
    - 정적 팩토리 생성자 추가

--- artcollector_back/src/main/java/com/dahoon/toy/artcollector/review/ReviewDto.java
+++ artcollector_back/src/main/java/com/dahoon/toy/artcollector/review/ReviewDto.java
@@ -5,18 +5,38 @@ import com.dahoon.toy.artcollector.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class ReviewDto {
     private String content;
     private Integer rating;
     private String gameId;
+    private String writer;
+    private String lastModifiedTime;
+
+    public ReviewDto(String content, int rating, String gameId) {
+        this.content = content;
+        this.rating = rating;
+        this.gameId = gameId;
+    }
 
     public static ReviewDto toDto(Review review) {
+        String modifiedTime = (review.getUpdatedTime() != null)
+                ? review.getUpdatedTime().toString()
+                : review.getCreatedTime().toString();
+
+        String writer = (review.getUser().isDeleted())
+                ? "탈퇴한 사용자"
+                : review.getUser().getNickname();
+
         return new ReviewDto(
                 review.getContent(),
                 review.getRating(),
-                review.getGameId());
+                review.getGameId(),
+                writer,
+                modifiedTime);
     }
 
     public Review toEntity(User user) {
